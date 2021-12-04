a6fc6cdf8cf7eebafb3f077a722b681526bb02e4
matthewvolny <matthew.volny@gmail.com>
2021-12-04 10:54:51 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Elements
+const pomodoroAppElement = document.getElementById("pomodoro-app");
+const pomodoroCounterDisplayElement = document.getElementById(
+  "pomodoroCounterDisplay"
+);
+const pomodoroFormElement = document.getElementsByClassName("pomorodoForm")[0];
+const minuteInputElement = document.getElementById("minutesInputText");
+const secondInputElement = document.getElementById("secondsInputText");
+//pomodoroErrorList
+const pomodoroListOfErrorsElement =
+  document.getElementById("pomodoroErrorList");
+const addPomodoroToListButton = document.getElementById("pomorodoForm-addBtn");
+const pomodoroListElement = document.getElementById("PomodoroList");
+const startOrPauseAPomodoroFromListButton = document.getElementById(
+  "pomorodoForm-StartPauseBtn"
+);
