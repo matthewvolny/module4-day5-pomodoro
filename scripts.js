17f9b4370fea59651e29e29b21afa1961a2f9ff8
matthewvolny <matthew.volny@gmail.com>
2021-12-04 14:02:01 -0500

    clock working, not showing 2 digits on display

--- scripts.js
+++ scripts.js
@@ -14,3 +14,62 @@ const pomodoroListElement = document.getElementById("PomodoroList");
 const startOrPauseAPomodoroFromListButton = document.getElementById(
   "pomorodoForm-StartPauseBtn"
 );
+
+let minutesInputGlobal;
+let secondsInputGlobal;
+
+minuteInputElement.addEventListener("input", (e) => {
+  addPomodoroToListButton.removeAttribute("disabled");
+  paddedMinutes = String(minuteInputElement.value).padStart(2, "0");
+  minuteInputElement.value = paddedMinutes;
+  let minutesInput = Number(minuteInputElement.value);
+  minutesInputGlobal = minutesInput;
+});
+
+secondInputElement.addEventListener("input", (e) => {
+  addPomodoroToListButton.removeAttribute("disabled");
+  paddedSeconds = String(secondInputElement.value).padStart(2, "0");
+  secondInputElement.value = paddedSeconds;
+  let secondsInput = Number(paddedSeconds);
+  console.log(typeof secondsInput);
+  secondsInputGlobal = secondsInput;
+});
+
+addPomodoroToListButton.addEventListener("click", (e) => {
+  startOrPauseAPomodoroFromListButton.removeAttribute("disabled");
+  pomodoroCounterDisplayElement.innerHTML = `${String(
+    minuteInputElement.value
+  ).padStart(2, "0")} : ${String(secondInputElement.value).padStart(2, "0")}`;
+});
+
+const changeImages = () => {
+  if (secondsInputGlobal > 1) {
+    secondsInputGlobal--;
+    pomodoroCounterDisplayElement.innerHTML = `${minutesInputGlobal} : ${secondsInputGlobal}`;
+  } else if (secondsInputGlobal === 1 && minutesInputGlobal !== 0) {
+    secondsInputGlobal = 60;
+    minutesInputGlobal--;
+    pomodoroCounterDisplayElement.innerHTML = `${minutesInputGlobal} : ${secondsInputGlobal}`;
+  } else {
+    pomodoroListOfErrorsElement.innerHTML = "TIME'S UP!";
+  }
+};
+
+startOrPauseAPomodoroFromListButton.addEventListener("click", (e) => {
+  setInterval(() => {
+    changeImages();
+  }, 1000);
+});
+
+// let array = [3, 5, 26, 4];
+
+// redBox.addEventListener("click", (e) => {
+//   redBox.classList.toggle("greenbox", array.length > 2);
+//   array.pop(); // changes back to red on the third click
+//   console.log(array); // [3, 5]
+// });
+
+//  document.addEventListener("click", (e) => {
+//    console.log("click detected");
+//    clearInterval(intervalForImages);
+//  });
